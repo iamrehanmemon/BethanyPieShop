024569489b86d7de2b5eee8c68d72d7f7f130acc
Rehan Memon <rehan4747@gmail.com>
2021-04-05 16:49:49 +0530

    Created The domain & Repository

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,3 +1,4 @@
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
             /*For MVC with default route*/
             services.AddMvc(option => option.EnableEndpointRouting = false);

e4e41a141dbfb7520a8b7b8651955fe050f87b3a
Rehan Memon <rehan4747@gmail.com>
2021-04-05 17:41:56 +0530

    Created Views, ViewModels for the Home Page

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+        //GET: /<controller>/
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+
+    }
+}
+
+
